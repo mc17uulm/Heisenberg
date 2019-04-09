36dd0f288b29b70b357de56fef92841704d2dce0
mc17uulm <marco.combosch@uni-ulm.de>
2019-04-09 13:23:14 +0200

    deleted unnecessary files; first full round

--- Assets/HeisenbergScene/Scripts/Config.cs
+++ Assets/HeisenbergScene/Scripts/Config.cs
@@ -76,7 +76,7 @@ public class Config {
 };
 
     //public static int CircleSize = 13;
-    public static int CircleSize = 3;
+    public static int CircleSize = 13;
 
     // Record clickes based on PAD or TRIGGER
     //public static ClickMode clickMode = ClickMode.TRIGGER;
